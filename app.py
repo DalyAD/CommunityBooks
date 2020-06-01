726e42aca34e38b847558aaf7280d8eae5bb638f
DalyAD <Addaly91@gmail.com>
2020-06-01 20:30:58 +0000

    fixing app error in heroku

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = "CommunityBooks"
 app.config["MONGO_URI"] = os.environ.get("SECRET_KEY")
-
 
 mongo = PyMongo(app)
 

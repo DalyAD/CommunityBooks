5d991fceb9a20f190e775f9e12e1d761972b0883
DalyAD <Addaly91@gmail.com>
2020-06-01 20:28:38 +0000

    fixing app error in heroku

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-if os.pathe.exists("env.py"):
+if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = "CommunityBooks"
-app.secret_key = os.environ.get("SECRET_KEY")
+app.config["MONGO_URI"] = os.environ.get("SECRET_KEY")
 
 
 mongo = PyMongo(app)
